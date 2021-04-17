0fce1b32f9bb12ce90e5563448b3d79d93f4735d
mw3155 <mw3155@protonmail.com>
2021-04-17 17:33:21 +0200

    🚧 progress circle example

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,115 @@
+/// Flutter code sample for CircularProgressIndicator
+
+// This example shows a [CircularProgressIndicator] with a changing value.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _MyStatefulWidgetState extends State<MyStatefulWidget>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    //controller.repeat(reverse: true);
+    controller.forward();
+    controller.addStatusListener(
+      (status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(controller.value);
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Linear progress indicator with a fixed color',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(
+              height: 300,
+              child: Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      child: CircularProgressIndicator(
+                        value: controller.value,
+                        strokeWidth: 10,
+                        semanticsLabel: 'Linear progress indicator',
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      alignment: FractionalOffset.center,
+                      child: Text("hi"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -50,7 +162,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: "/",
       routes: {
-        "/": (context) => HomePage(),
+        "/": (context) => CircularProgress(),
         "timer": (context) => TimerPage(),
         "meme": (context) => MemePage(),
         "work": (context) => WorkPage(),
@@ -250,6 +362,7 @@ class _TimerPageState extends State<TimerPage>
   @override
   void initState() {
     super.initState();
+
     _controller = AnimationController(
       vsync: this,
       duration: isExtraTime
@@ -543,3 +656,60 @@ class ZebraPage extends StatelessWidget {
     );
   }
 }
+
+/// This is the stateful widget that the main application instantiates.
+class CircularProgress extends StatefulWidget {
+  const CircularProgress({Key? key}) : super(key: key);
+
+  @override
+  _CircularProgressState createState() => _CircularProgressState();
+}
+
+/// This is the private State class that goes with CircularProgress.
+/// AnimationControllers can be created with `vsync: this` because of TickerProviderStateMixin.
+class _CircularProgressState extends State<CircularProgress>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..addListener(() {
+        setState(() {});
+      });
+    controller.repeat(reverse: true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              'Linear progress indicator with a fixed color',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            CircularProgressIndicator(
+              value: controller.value,
+              semanticsLabel: 'Linear progress indicator',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+*/
